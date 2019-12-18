17d2cfa036388ebf336f9e11041a3ba6abd1acdc
yihua <yihua@ecquaria.com>
2019-12-18 18:06:00 +0800

    test3

--- user/src/main/java/cn/yihua/cloud/demo/user/UserApplication.java
+++ user/src/main/java/cn/yihua/cloud/demo/user/UserApplication.java
@@ -1,20 +1,25 @@
 package cn.yihua.cloud.demo.user;
 
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@EnableCircuitBreaker
-//@EnableHystrixDashboard
 public class UserApplication {
 
     @Bean
@@ -23,8 +28,49 @@ public class UserApplication {
         return new RestTemplate();
     }
 
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .timeLimiterConfig(TimeLimiterConfig.custom()
+                                .timeoutDuration(Duration.ofSeconds(4)).build())
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .build());
+    }
+
+//    @Bean
+//    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+//
+//        Customizer<Resilience4JCircuitBreakerFactory> aa = new Customizer<Resilience4JCircuitBreakerFactory>(){
+//            @Override
+//            public void customize(Resilience4JCircuitBreakerFactory tocustomize){
+//                tocustomize.configureDefault(
+//                        id -> new Resilience4JConfigBuilder(id)
+//                                .timeLimiterConfig(TimeLimiterConfig.custom()
+//                                        .timeoutDuration(Duration.ofSeconds(4)).build())
+//                                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+//                                .build());
+//            }
+//        };
+//        return aa;
+//    }
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
+
+
+
+//    @Bean
+//    public Customizer<HystrixCircuitBreakerFactory> defaultConfig() {
+//        return factory -> factory.configureDefault(id -> HystrixCommand.Setter
+//                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(id))
+//                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(3000)));
+//    }
+
+
 
 }
--- user/src/main/java/cn/yihua/cloud/demo/user/service/ActiveService.java
+++ user/src/main/java/cn/yihua/cloud/demo/user/service/ActiveService.java
@@ -2,44 +2,36 @@ package cn.yihua.cloud.demo.user.service;
 
 
 import cn.yihua.cloud.demo.user.controller.UserFeign;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
 public class ActiveService {
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
     private UserFeign userFeign;
 
-    private static final String BACKEND = "backendA";
-
-    public String failure(Long id,Throwable throwable){
-        return "调用服务出错时，则执行该方法 错误信息:"+throwable.getMessage();
+    public String activeFirstRegistry(Long id) {
+        return cbFactory.create("activeFirstRegistry").run(() -> restTemplate.postForObject("http://activity/firstLoginActivity", id, String.class), throwable -> "访问服务出错1");
     }
 
-    @CircuitBreaker(name = BACKEND,fallbackMethod = "failure")
-//    @HystrixCommand(commandKey="registryUser",fallbackMethod = "fallback")
-    public String activeFirstRegistry(Long id){
-        return restTemplate.postForObject("http://activity/firstLoginActivityTimeOut", id, String.class);
+
+    public String activeFirstRegistryError(Long id) {
+        return cbFactory.create("activeFirstRegistryErrorTimeOut").run(() -> restTemplate.postForObject("http://activity/firstLoginActivityTimeOut", id, String.class), throwable -> "访问服务出错2");
     }
 
-    @CircuitBreaker(name = "backendY",fallbackMethod = "failure")
-//    @HystrixCommand(commandKey="registryUser1",fallbackMethod = "fallback")
-    public String activeFirstRegistryError(Long id){
-        return restTemplate.postForObject("http://activity/firstLoginActivityError", id, String.class);
+
+    public String activeFirstRegistryByFeign(Long id) {
+        return cbFactory.create("activeFirstRegistryByFeign").run(() -> userFeign.firstLoginError(id), throwable -> "访问服务出错3"+throwable.getMessage());
     }
 
-    @CircuitBreaker(name = BACKEND,fallbackMethod = "failure")
-    //@HystrixCommand(commandKey="registryUser2",fallbackMethod = "fallback")
-    public String activeFirstRegistryByFeign(Long id){
-        return userFeign.firstLoginError(id);
-    }
-
-    public String fallback(Long id){
-        return "调用出错,启用备用方法:"+id;
-    }
 }
